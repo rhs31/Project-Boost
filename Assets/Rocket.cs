99dfb438f2556e3228a5c8eea2662908c02681d2
rahb19 <r.silverberg@yahoo.com>
2018-08-02 16:09:53 -0400

    Ship rotation and thrust

---
+++ Assets/Rocket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) //can thrust while rotating
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
